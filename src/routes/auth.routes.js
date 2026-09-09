b5acc3f384342dae621d9b87356999f5127486e3
wordpressprepmed <wordpressprepmed@gmail.com>
2026-09-09 13:17:36 +0530

    login api added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -3,6 +3,10 @@ const authcontroller = require("../controllers/auth.controller")
 
 const router = express.Router()
 
+/*Register Route*/
 router.post("/register",authcontroller.userRegisterController)
+
+/*Login Route*/
+router.post("/login",authcontroller.userLoginController)
 
 module.exports = router
