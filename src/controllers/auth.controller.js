4809428bb97590d341bb7a9cd5f6fbae80c85c7a
BishalDey908 <ronydey34567@gmail.com>
2026-09-15 18:08:09 +0530

    send-mail-system added

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,8 @@
 const userModel = require("../models/user.model")
 const jwt = require("jsonwebtoken")
 
+const emailService = require("../services/email.service");
+
 async function userRegisterController(req, res) {
     const { email, password, name } = req.body;
 
@@ -35,6 +37,9 @@ async function userRegisterController(req, res) {
         },
         token
     })
+
+
+    await emailService.sendRegistrationEmail(user.email,user.name);
 }
 
 async function userLoginController(req, res) {
