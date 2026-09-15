18eac26f15bab412166e0a7bdd509ddfbae768c2
BishalDey908 <ronydey34567@gmail.com>
2026-09-15 10:47:42 +0530

    login-route-added

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,9 +4,20 @@ const authcontroller = require("../controllers/auth.controller")
 const router = express.Router()
 
 /*Register Route*/
+
+/**     
+ * * - @route POST /api/auth/register
+ * * - @desc Register a new user
+ * * - @access Public
+*/
 router.post("/register",authcontroller.userRegisterController)
 
 /*Login Route*/
+/**     
+ * * - @route POST /api/auth/login
+ * * - @desc Login a user
+ * * - @access Public
+*/
 router.post("/login",authcontroller.userLoginController)
 
 module.exports = router
